dfc9704b4e53a59c7816e828f355b4b9b917ff2a
Todd Southen <48103957+toddsouthenbentley@users.noreply.github.com>
2021-09-08 16:53:08 -0700

    Renamed repos/packages to be consistent.

---
+++ src/mobile-ui-react.ts
@@ -0,0 +1,29 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) 2020 Bentley Systems, Incorporated. All rights reserved.
+*--------------------------------------------------------------------------------------------*/
+// this file will contain all the exports for this "package"
+export * from "./mobile-ui-react/MobileUi";
+export * from "./mobile-ui-react/MobileUiContent";
+// NOTE: ScrollableWithFades must precede anything using fadeClassName, so it is being put near the top.
+export * from "./mobile-ui-react/ScrollableWithFades";
+// NOTE: NavigationPanel MUST precede anything that might use its className property.
+export * from "./mobile-ui-react/NavigationPanel";
+export * from "./mobile-ui-react/IconImage";
+export * from "./mobile-ui-react/VisibleBackButton";
+export * from "./mobile-ui-react/ActionSheetButton";
+export * from "./mobile-ui-react/TabBar";
+export * from "./mobile-ui-react/CircularButton";
+export * from "./mobile-ui-react/ResizablePanel";
+export * from "./mobile-ui-react/PanelHeader";
+export * from "./mobile-ui-react/PanViewport";
+export * from "./mobile-ui-react/RotateViewport";
+export * from "./mobile-ui-react/BottomPanel";
+export * from "./mobile-ui-react/TileGrid";
+export * from "./mobile-ui-react/CenterDiv";
+export * from "./mobile-ui-react/Suggestion";
+export * from "./mobile-ui-react/CountNotification";
+export * from "./mobile-ui-react/AcceptButton";
+export * from "./mobile-ui-react/HorizontalPicker";
+export * from "./mobile-ui-react/DatePicker";
+export * from "./mobile-ui-react/ModalEntryFormDialog";
+export * from "./mobile-ui-react/TabStrip";
--- syncToTestApp.sh
+++ syncToTestApp.sh
@@ -3,12 +3,14 @@ if [ "$1" != "" ]; then
   appDir=$1
 elif [ "$ITM_TEST_APP_DIR" != "" ]; then
   appDir=$ITM_TEST_APP_DIR
+elif [ -d "../mobile-sdk-samples/iOS/MobileStarter/react-app" ]; then
+  appDir=../mobile-sdk-samples/iOS/MobileStarter/react-app
 else
   echo Destination must be specified either as argument or in ITM_TEST_APP_DIR
   echo variable.
   exit 1
 fi
-destDir=${appDir}/node_modules/@itwin/mobileui-react
+destDir=${appDir}/node_modules/@itwin/mobile-ui-react
 # If destDir is a symlink, it probably came from nmp link or a file: package.json reference. Delete the link.
 [ -L "${destDir}" ] && rm "${destDir}"
 [ -d "${destDir}" ] || mkdir -p "${destDir}"
