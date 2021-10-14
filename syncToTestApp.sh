d66c91318f743252c384f1259fb0e115a487fe54
Travis.Cobbs <>
2021-10-14 14:50:37 -0700

    Fix syncToTestApp.sh to work when destination module is installed by npm.

--- syncToTestApp.sh
+++ syncToTestApp.sh
@@ -13,5 +13,7 @@ fi
 destDir=${appDir}/node_modules/@itwin/mobile-ui-react
 # If destDir is a symlink, it probably came from nmp link or a file: package.json reference. Delete the link.
 [ -L "${destDir}" ] && rm "${destDir}"
+# If node_modules exists in destDir, that means that the module was installed by npm; nuke the install.
+[ -d "${destDir}/node_modules" ] && rm -rf "${destDir}"
 [ -d "${destDir}" ] || mkdir -p "${destDir}"
 rsync -aL --delete lib LICENSE.md package.json README.md "${destDir}/"
