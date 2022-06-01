e2c42a8124229027060ebef5f539980fac0630f3
Simi Hartstein <simi.hartstein@bentley.com>
2022-06-01 16:49:31 -0400

    Consistent spacing

--- syncToTestApp.ps1
+++ syncToTestApp.ps1
@@ -1,9 +1,9 @@
 
-if( $args[0] -ne $null ) {
+if ($args[0] -ne $null) {
   $appDir = $args[0]
-} elseif ( $Env:ITM_TEST_APP_DIR -ne $null ) {
+} elseif ($Env:ITM_TEST_APP_DIR -ne $null) {
   $appDir = $Env:ITM_TEST_APP_DIR
-} elseif ( Test-Path -Path "../mobile-samples/cross-platform/react-app" ) {
+} elseif (Test-Path -Path "../mobile-samples/cross-platform/react-app") {
   $appDir = "../mobile-samples/cross-platform/react-app"
 } else {
   echo "Destination must be specified either as argument or in ITM_TEST_APP_DIR variable."
@@ -15,24 +15,24 @@ $destDir = "$appDir/node_modules/@itwin/mobile-ui-react"
 
 # If destDir is a symlink, it probably came from npm link or a file: package.json reference. Delete the link.
 $isSymlink = ((Get-Item $destDir).Attributes.ToString() -match "ReparsePoint")
-if($isSymlink) {
+if ($isSymlink) {
   Get-Item $destDir | %{$_.Delete()}
 }
 
 # If node_modules exists in destDir, that means that the module was installed by npm; nuke the install.
-if(Test-Path -Path "$destDir/node_modules") {
+if (Test-Path -Path "$destDir/node_modules") {
   Remove-Item $destDir -Recurse
 }
 
-if( -Not (Test-Path -Path $destDir) ) {
+if (-Not (Test-Path -Path $destDir)) {
   New-Item -ItemType "directory" -Path $destDir
 }
 
 # sync the wanted subdirectories 
-foreach ( $dir in "lib", "src" ) {
+foreach ($dir in "lib", "src") {
   ROBOCOPY $dir "$destDir/$dir" /MIR
 }
 # Copy the wanted files
-foreach ($file in "LICENSE.md", "package.json", "README.md" ) {
+foreach ($file in "LICENSE.md", "package.json", "README.md") {
   Copy-Item $file -Destination $destDir
 }
