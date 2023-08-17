b369db7f7330545c75b6e2a40deadafdb748d7f0
Travis.Cobbs <77415528+tcobbs-bentley@users.noreply.github.com>
2023-08-17 15:49:03 -0700

    Update to latest @itwin/eslint-plugin

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+const iTwinPlugin = require("@itwin/eslint-plugin");
+
+module.exports = [
+  {
+    ignores: [
+      "src/mobile-ui-react/images-tsx/**"
+    ],
+    files: ["**/*.{ts,tsx}"],
+    ...iTwinPlugin.configs.iTwinjsRecommendedConfig,
+  },
+  {
+    ignores: [
+      "src/mobile-ui-react/images-tsx/**"
+    ],
+    files: ["**/*.{ts,tsx}"],
+    ...iTwinPlugin.configs.jsdocConfig,
+  },
+  {
+    ignores: [
+      "src/mobile-ui-react/images-tsx/**"
+    ],
+    files: ["**/*.tsx"],
+    ...iTwinPlugin.configs.uiConfig,
+  },
+  {
+    rules: {
+      "nonblock-statement-body-position": "off",
+      "@itwin/no-internal-barrel-imports": "off",
+      "jsx-a11y/click-events-have-key-events": "off",
+      "jsx-a11y/no-static-element-interactions": "off"
+    }
+  }
+];
